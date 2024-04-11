27a6997c27525d15f4f08cb018b697471f5ffe78
wendietibrias <wendietibrias@gmail.com>
2024-04-11 12:43:54 +0700

    fix: responsive in mobile screen

---
+++ src/common/constants/brands.ts
@@ -0,0 +1,68 @@
+import brand1 from "../../assets/images/brand1.png";
+import brand2 from "../../assets/images/brand2.png";
+import brand3 from "../../assets/images/brand3.png";
+import brand4 from "../../assets/images/brand4.png";
+import brand5 from "../../assets/images/brand5.png";
+import brand6 from "../../assets/images/brand6.png";
+import brand7 from "../../assets/images/brand7.png";
+import brand8 from "../../assets/images/brand8.png";
+import brand9 from "../../assets/images/brand9.png";
+import brand10 from "../../assets/images/brand10.png";
+import brand11 from "../../assets/images/brand11.png";
+import brand12 from "../../assets/images/brand12.png";
+import brand13 from "../../assets/images/brand13.png";
+import brand14 from "../../assets/images/brand14.png";
+import brand15 from "../../assets/images/brand15.png";
+import brand16 from "../../assets/images/brand16.png";
+
+
+export default [
+    {
+        image: brand1 
+    },
+    {
+        image:brand2,
+    },
+    {
+        image:brand3
+    },
+    {
+        image:brand4
+    },
+    {
+        image:brand5
+    },
+    {
+        image:brand6
+    },
+    {
+        image: brand7
+    },
+    {
+        image:brand8,
+    },
+    {
+        image:brand9
+    },
+    {
+        image:brand10
+    },
+    {
+        image:brand11
+    },
+    {
+        image:brand12
+    },
+    {
+        image:brand13,
+    },
+    {
+        image:brand14
+    },
+    {
+        image:brand15
+    },
+    {
+        image:brand16
+    }
+]
--- src/components/home/partner/PartnerSection.tsx
+++ src/components/home/partner/PartnerSection.tsx
@@ -1,20 +1,15 @@
+import brands from '../../../common/constants/brands';
 import './style.scss';
 
 
 const PartnerSection = () => {
-
-    const brandLists = [];
-
-    for(let i = 0; i < 16; i++) {
-        brandLists.push(`src/assets/images/brand${i+1}.png`);
-    }
 
     return (
         <section className='partner--container'>
             <div className='partner--container-content'>
                 <h1>Findtrend make +1000 Startup grow</h1>
                 <div className='partner--container-content-grids'>
-                    {brandLists.map((item:string,idx:number)=> <img src={item} alt={item} key={idx}/>)}
+                    {brands.map((item:{image:string},idx:number)=> <img src={item.image} alt={'brand'} key={idx}/>)}
                 </div>
             </div>
         </section>
